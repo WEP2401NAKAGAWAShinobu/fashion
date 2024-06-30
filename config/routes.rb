6f1d3ca4e5959a20364d50ebbb28cd1b7135a5c0
s.nakagawa <saku.shi.katsura623@gmail.com>
2024-06-30 15:54:56 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   post 'guest_login', to: 'sessions#guest_login'
   delete 'guest_logout', to: 'sessions#guest_logout'
 
+  get '*path', to: 'application#render505'
+ 
+  resources :cloth_coordinates, only: [:index, :show, :destroy]
+
 
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
   resources :cloths
@@ -18,9 +22,8 @@ Rails.application.routes.draw do
     collection do
       get 'random'
     end
+    resources :cloth_coordinates, only: [:index, :show, :destroy]
   end
-  resources :cloth_coordinates, only: [:index, :show, :destroy]
-  #get '*not_found' => 'application#routing_error'
-  #post '*not_found' => 'application#routing_error'
+  
 end
   
